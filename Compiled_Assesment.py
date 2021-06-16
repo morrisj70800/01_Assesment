a355bca44163e69c80e858bc22909e5cce8e9949
morrisj70800 <78512092+morrisj70800@users.noreply.github.com>
2021-06-16 12:56:55 +1200

    Updated Start_GUI
    Removed roman and greek options.

--- Compiled_Assesment.py
+++ Compiled_Assesment.py
@@ -11,13 +11,15 @@ class Start:
         self.start_frame.grid()
 
         # Quiz Heading (row 0)
-        self.legendary_quiz_label = Label(self.start_frame, text="Legendary God Quiz",
-                                       font="Arial 19 bold")
+        self.legendary_quiz_label = Label(self.start_frame, text="Norse God Quiz",
+                                          font="Arial 19 bold")
         self.legendary_quiz_label.grid(row=0)
         # Initial Instructions (row 1)
         self.legendaryquiz_instructions = Label(self.start_frame, font="arial 10 italic",
-                                          text="Please Choose Your Gods ",
-                                          wrap=275, justify=LEFT, padx=10, pady=10)
+                                                text="Please Press play. you will"
+                                                     "be taken to a quiz full of norse"
+                                                     "gods ",
+                                                     wrap=275, justify=LEFT, padx=10, pady=10)
         self.legendaryquiz_instructions.grid(row=1)
 
         # GUI Setup
@@ -30,31 +32,20 @@ class Start:
         self.start_frame = Frame(self.start_frame)
         self.start_frame.grid(row=3)
 
-        # Buttons go here
+        # Button goes here
         button_font = "Arial 12 bold"
-        # Roman God  button (row 3)
-        self.Roman_god_button = Button(self.start_frame, text="Roman",
-                                       command=lambda: self.to_game(1),
-                                       font=button_font, bg="Yellow")
-        self.Roman_god_button.grid(row=0, column=0, pady=10)
-
-        # Greek God Button
-        self.Greek_god_button = Button(self.start_frame, text="Greek",
-                                       command=lambda: self.to_game(2),
-                                       font=button_font, bg="Yellow")
-        self.Greek_god_button.grid(row=0, column=1, padx=5, pady=10)
 
         # Norse God Button
-        self.Norse_god_button = Button(self.start_frame, text="Norse",
-                                       command=lambda: self.to_game(3),
-                                       font=button_font, bg="Yellow")
-        self.Norse_god_button.grid(row=0, column=2, pady=10)
+        self.Play_button = Button(self.start_frame, text="Play",
+                                  command=lambda: self.to_game(1),
+                                  font=button_font, bg="Yellow")
+        self.Play_button.grid(row=0, column=3, pady=10)
 
         # Boxes go here (row 2)
         self.box_frame = Frame(self.game_frame)
         self.box_frame.grid(row=2, pady=10)
 
-        photo = PhotoImage(file="Zeus.gif")
+        photo = PhotoImage(file="Odin.gif")
 
         self.god1_label = Label(self.box_frame, image=photo,
                                 padx=10, pady=10)
@@ -62,16 +53,8 @@ class Start:
         self.god1_label.grid(row=0, column=0)
 
 
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     root = Tk()
-    root.title("Legendary God Quiz")
+    root.title("Norse God Quiz")
     play = Start(root)
     root.mainloop()
