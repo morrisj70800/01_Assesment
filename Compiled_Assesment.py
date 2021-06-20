dddcc27230322ee1ad7abdb9910111aff9f10acd
morrisj70800 <78512092+morrisj70800@users.noreply.github.com>
2021-06-21 09:48:18 +1200

    Added Help Class.
    + messed around with game gui

--- Compiled_Assesment.py
+++ Compiled_Assesment.py
@@ -18,7 +18,8 @@ class Start:
         self.legendary_quiz_instructions = Label(self.start_frame, font="arial 10 italic",
                                                 text="Please Press play. you will"
                                                      "be taken to a quiz full of norse"
-                                                     "gods ",
+                                                     "gods this quiz is a 15 question"
+                                                     " quiz ",
                                                      wrap=275, justify=LEFT, padx=10, pady=10)
         self.legendary_quiz_instructions.grid(row=1)
 
@@ -69,10 +70,45 @@ class Start:
 
 
 class Help:
-    def __init__(self,partner):
-        print("You asked for help")
+    def __init__(self, partner):
+        background = "Yellow"
 
+        # disable help button
         partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # If users press 'x' cross at the top, closes help and 'releases' help button.
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # Set up GUI Frame
+        self.help_frame = Frame(self.help_box, bg=background)
+        self.help_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help",
+                                 font=("Aerial", "24", "bold",),
+                                 bg=background)
+        self.how_heading.grid(row=0)
+
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame, text="This quiz is a 15 question quiz"
+                                                     "this is a straight forward quiz"
+                                                     "there are 4 answers and only one"
+                                                     "of them is correct. ", font="yellow",
+                                bg=background, justify=LEFT,wrap=350)
+        self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg="blue", fg="white",
+                                  font="Helvetica" "10" "bold", command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 
 class Game:
@@ -93,31 +129,31 @@ class Game:
         self.top_answers_frame = Frame(self.game_box)
         self.top_answers_frame.grid(row=2)
 
-        # Top level answers buttons row 2.0
-        self.top_left_answer_button = Button(self.top_answers_frame, text="Top left",
+        # Top level answers buttons row 1.0
+        self.top_left_answer_button = Button(self.top_answers_frame, text="Answer 1",
                                              font="Aerial 10 bold", padx=5, pady=5,
                                              command=lambda: self.reveal_answer(0))
         self.top_left_answer_button.grid(column=0, row=0)
 
-        self.top_right_answer_button = Button(self.top_answers_frame, text="Top right",
+        self.top_right_answer_button = Button(self.top_answers_frame, text="Answer 2",
                                               font="Aerial 10 bold", padx=5, pady=5,
                                               command=lambda: self.reveal_answer(1))
         self.top_right_answer_button.grid(column=1, row=0)
 
-        # Bottom level answers buttons row 2.1
-        self.bottom_left_answer_button = Button(self.top_answers_frame, text="Bottom left",
+        # Bottom level answers buttons row 2.0
+        self.bottom_left_answer_button = Button(self.top_answers_frame, text="Answer 3",
                                                 font="Aerial 10 bold", padx=5, pady=5,
                                                 command=lambda: self.reveal_answer(2))
         self.bottom_left_answer_button.grid(column=0, row=1)
 
-        self.bottom_right_answer_button = Button(self.top_answers_frame, text="Bottom right",
+        self.bottom_right_answer_button = Button(self.top_answers_frame, text="Answer 4",
                                                  font="Aerial 10 bold", padx=5, pady=5,
                                                  command=lambda: self.reveal_answer(3))
         self.bottom_right_answer_button.grid(column=1, row=1)
 
+
     def reveal_answer(self, location):
       # Print corresponding number based on location
-      # TL = 0 TR =1 BL = 2 BR = 3
       print(location)
 
 
